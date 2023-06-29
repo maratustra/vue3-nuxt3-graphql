fa5629f7f51cafb8746f3b1dc672848d622c67dc
Alena Maramygina <maratustraa@gmail.com>
2023-06-29 23:13:34 +0400

    build(frontned): add lint

--- app.vue
+++ app.vue
@@ -1,5 +1,13 @@
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+  <div class="wrapper-text">123</div>
 </template>
+
+<style lang="scss" scoped>
+.wrapper {
+  &-text {
+    width: 10px;
+    height: 10px;
+    color: red;
+  }
+}
+</style>
